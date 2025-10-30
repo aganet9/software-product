5d9464414d518fc88905c63d390d986bb43a8f16
AGANETHED <astcvetkov1@chsu.ru>
2025-10-30 17:17:08 +0300

    first commit

---
+++ src/main/java/ru/chsu/software_product/SoftwareProductApplication.java
@@ -0,0 +1,13 @@
+package ru.chsu.software_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/chsu/software_product/SoftwareProductApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chsu.software_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
