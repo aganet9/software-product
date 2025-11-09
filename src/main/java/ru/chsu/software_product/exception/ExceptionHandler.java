43743e5ba9f4ac633d9047f312fa6b54a14c98d4
AGANETHED <astcvetkov1@chsu.ru>
2025-11-09 19:51:16 +0300

    feat: add exception handler

---
+++ src/main/java/ru/chsu/software_product/exception/ExceptionHandler.java
@@ -0,0 +1,45 @@
+package ru.chsu.software_product.exception;
+
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExceptionHandler {
+
+    public void handleException(Exception ex) {
+        if (UI.getCurrent() == null) return;
+        UI.getCurrent().access(() -> {
+            switch (ex) {
+                case DeveloperExistException developerExistException ->
+                        Notification.show(ex.getMessage(), 5000, Notification.Position.MIDDLE)
+                                .addThemeVariants(NotificationVariant.LUMO_WARNING);
+                case DeveloperNotFoundException developerNotFoundException ->
+                    Notification.show(ex.getMessage(), 5000, Notification.Position.MIDDLE)
+                            .addThemeVariants(NotificationVariant.LUMO_WARNING);
+                case SoftwareProductNotFoundException softwareProductNotFoundException ->
+                        Notification.show(ex.getMessage(), 5000, Notification.Position.MIDDLE)
+                                .addThemeVariants(NotificationVariant.LUMO_WARNING);
+                case SoftwareProductExistException softwareProductExistException ->
+                    Notification.show(ex.getMessage(), 5000, Notification.Position.MIDDLE)
+                            .addThemeVariants(NotificationVariant.LUMO_WARNING);
+                case RequirementTypeException requirementTypeException ->
+                        Notification.show(ex.getMessage(), 5000, Notification.Position.MIDDLE)
+                                .addThemeVariants(NotificationVariant.LUMO_WARNING);
+                case LicenseNotFoundException licenseNotFoundException ->
+                        Notification.show(ex.getMessage(), 5000, Notification.Position.MIDDLE)
+                                .addThemeVariants(NotificationVariant.LUMO_WARNING);
+                case PatchNotFoundException patchNotFoundException ->
+                        Notification.show(ex.getMessage(), 5000, Notification.Position.MIDDLE)
+                                .addThemeVariants(NotificationVariant.LUMO_WARNING);
+                case SystemRequirementNotFoundException systemRequirementNotFoundException ->
+                        Notification.show(ex.getMessage(), 5000, Notification.Position.MIDDLE)
+                                .addThemeVariants(NotificationVariant.LUMO_WARNING);
+                default -> Notification.show("Произошла непредвиденная ошибка: " + ex.getMessage(),
+                        5000, Notification.Position.MIDDLE)
+                        .addThemeVariants(NotificationVariant.LUMO_ERROR);
+            }
+        });
+    }
+}
